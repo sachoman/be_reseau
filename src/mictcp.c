cec181f1d904be191570af7f74a119d88672c62a
sachoman <sachadarthenucq@gmail.com>
2022-03-23 18:28:04 +0100

    v2 fini

--- src/mictcp.c
+++ src/mictcp.c
@@ -1,7 +1,7 @@
 #include <mictcp.h>
 #include <api/mictcp_core.h>
 
-#define nb_envois_max 10
+#define nb_envois_max 10000
 
 
 typedef struct liste_sock_addr{
@@ -68,7 +68,7 @@ int mic_tcp_socket(start_mode sm)
    if (result ==-1){
        return -1;
    }
-   set_loss_rate(0);
+   set_loss_rate(30);
    result = add_sock_list(0,&liste_socket_addresses);
    return result;
 }
@@ -106,32 +106,45 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
        return -1;
     }
     (pointeur_courant->addr_distante) = *addr;
+    /*
     int j = 0;
     mic_tcp_pdu synack;
-    j = IP_recv(&synack, addr, 4);
-    if ((j== -1) || ((synack.header).syn != 1)) {
-        printf("connexion non détectée\n");
-        exit(1);
-    }
     int i=-1;
-    synack.header.syn = 1;
-    synack.header.ack = 1;
-    synack.header.source_port = (pointeur_courant->sock_local).addr.port;
-    synack.header.dest_port = (pointeur_courant->addr_distante).port;
-    mic_tcp_pdu ack;
     int nb_envois = 0;
     while (i==-1){
         if (nb_envois > nb_envois_max){
             printf("erreur trop d'envois avec échec \n");
             exit(1);
         }
+        j = IP_recv(&synack, addr, 1000);
+        nb_envois++;
+        if ((synack.header).syn == '1') {
+            i=0;
+        }
+    }
+    synack.header.syn = '1';
+    synack.header.ack = '1';
+    synack.payload.data = NULL;
+    synack.payload.size = 0;
+    synack.header.source_port = (pointeur_courant->sock_local).addr.port;
+    synack.header.dest_port = (pointeur_courant->addr_distante).port;
+    mic_tcp_pdu ack;
+    i = -1;
+    nb_envois = 0;
+    while (i==-1){
+        if (nb_envois > nb_envois_max){
+            printf("erreur trop d'envois avec échec \n");
+            exit(1);
+        }
         IP_send(synack,*addr);
-                nb_envois ++;
-        i = IP_recv(&ack, addr, 4);
-        if ((ack.header).ack ==0){
+        nb_envois ++;
+        i = IP_recv(&ack, addr, 1000);
+        if ((ack.header).ack =='0'){
             i = -1;
         }
-    }   
+    }
+    printf("connexion acceptée\n") ;
+    */ 
     (pointeur_courant -> pe_a)=0;
     (pointeur_courant -> sock_local).state = ESTABLISHED;
     return 0;
@@ -151,31 +164,33 @@ int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
        return -1;
     }
     (pointeur_courant->addr_distante) = addr;
+    /*
     int i=-1;
-    mic_tcp_header header_syn;
-    header_syn.syn = 1;
-    header_syn.source_port = (pointeur_courant->sock_local).addr.port;
-    header_syn.dest_port = (pointeur_courant->addr_distante).port;
     mic_tcp_pdu syn;
-    syn.header=header_syn;
+    syn.header.syn ='1';
+    syn.header.source_port = (pointeur_courant->sock_local).addr.port;
+    syn.header.dest_port = (pointeur_courant->addr_distante).port;
+    syn.payload.data = NULL; 
+    syn.payload.size = 0; 
     mic_tcp_pdu synack;
     int nb_envois = 0;
+    printf("avant while \n");
     while (i==-1){
         if (nb_envois > nb_envois_max){
             printf("erreur trop d'envois avec échec \n");
             exit(1);
         }
+        printf("avant envoi \n");
         IP_send(syn,addr);
+        printf("apres envoi \n");
         nb_envois ++;
-        i = IP_recv(&synack, &addr, 4);
-        if (i!=-1){
-            if ((synack.header.ack != 1) || (synack.header.syn !=1)){
-                i = -1;
-            }
-        }
+        i = IP_recv(&synack, &addr, 1000);
+        printf("%d",i);
+        // a vérifier que c'est un synack
     }
-    syn.header.ack = 1;
+    syn.header.ack = '1';
     IP_send(syn,addr);
+    */
     (pointeur_courant -> pe_a)=0;
     (pointeur_courant -> sock_local).state = ESTABLISHED;
     return 0;
@@ -197,7 +212,8 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
        return -1;
     }
     header.seq_num = (pointeur_courant -> pe_a);
-    header.source_port = (pointeur_courant->addr_distante).port; /* numéro de port source */
+    header.source_port = (pointeur_courant->sock_local.addr).port; /* numéro de port source */
+    header.dest_port = (pointeur_courant->addr_distante).port;
     /*on a les infos du connect
 
     connect -> dest_port;
@@ -227,17 +243,23 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
             printf("erreur trop d'envois avec échec \n");
             exit(1);
         }
+        printf("on va envoyer\n");
         if ((nb_envoye = IP_send(pdu, (pointeur_courant->addr_distante)))==-1){
             printf("erreur envoi\n");
             exit(1);
         }
         nb_envois++;
-        j = IP_recv(ack,(pointeur_courant->addr_distante),4);
+        j = IP_recv(&ack,&(pointeur_courant->addr_distante),8);
+        printf("taille : %d\n", j);
         if (j==-1){
             printf("ACK non reçu, réenvoi des données\n");
-        }else{
-            if((ack.header.ack == '1')&&(ack.header.ack_num = pointeur_courant.pe_a)){
-                i=-1;
+        }
+        else{
+            printf("ack 1 ? : %c\n",ack.header.ack);
+            printf("n0 seq recu : %d\n",ack.header.ack_num);
+            printf("n0 seq attendu : %d\n",pointeur_courant->pe_a);
+            if((ack.header.ack == '1')&&(ack.header.ack_num == pointeur_courant->pe_a)){
+                i=0;
             }
         }
     }
@@ -286,10 +308,17 @@ void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
     /* ici int fd =0 pour l'instant*/
     fd_to_pointeur(0,&liste_socket_addresses);
     mic_tcp_pdu ack;
+
     if ((pointeur_courant->pe_a == pdu.header.seq_num)){
         //c'est bon
         //envoi ACK
-        ack.header.ack = pointeur_courant->pe_a;
+        printf("on envoi un ack avec message correct\n");
+        ack.header.ack ='1';
+        ack.header.ack_num = pointeur_courant->pe_a;
+        ack.header.source_port = pointeur_courant->sock_local.addr.port;
+        ack.header.dest_port = pointeur_courant->addr_distante.port;
+        ack.payload.data = NULL; 
+        ack.payload.size = 0; 
         //nouveau pa
         pointeur_courant->pe_a = 1 -(pointeur_courant->pe_a);
         IP_send(ack,pointeur_courant->addr_distante);
@@ -297,7 +326,13 @@ void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
     }
     else{
         //envoi de l'ancien pdu
-        ack.header.ack = 1 - pointeur_courant->pe_a;
+        ack.header.ack ='1';
+        ack.header.ack_num = 1 - pointeur_courant->pe_a;
+        printf("envoi ancien ack %d\n",ack.header.ack_num);
+        ack.header.source_port = pointeur_courant->sock_local.addr.port;
+        ack.header.dest_port = pointeur_courant->addr_distante.port;
+        ack.payload.data = NULL; 
+        ack.payload.size = 0;
         IP_send(ack,pointeur_courant->addr_distante);
     }
 }
